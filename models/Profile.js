b8dfbfffea7f39a3b3aed07bce9d11afa30cfa49
hmd-ltf <mhmd.ltf786@gmail.com>
2021-07-10 18:08:35 +0500

    Create User and Profile Model

---
+++ models/Profile.js
@@ -0,0 +1,79 @@
+const mongoose = require('mongoose');
+
+const profileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+  },
+  profilePic: {
+    type: String,
+  },
+  name: {
+    type: String,
+  },
+  briefSummary: {
+    type: String,
+  },
+
+  work: [
+    {
+      title: {
+        type: String,
+        required: true,
+      },
+      company: {
+        type: String,
+        required: true,
+      },
+      from: {
+        type: Date,
+        required: true,
+      },
+      to: {
+        type: Date,
+      },
+    },
+  ],
+  education: [
+    {
+      institution: {
+        type: String,
+        required: true,
+      },
+      from: {
+        type: Date,
+        required: true,
+      },
+      to: {
+        type: Date,
+      },
+    },
+  ],
+  totalVisits: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  lastActiveTime: {
+    type: Date,
+    default: Date.now,
+  },
+
+  messages: [
+    {
+      name: {
+        type: String,
+        required: true,
+      },
+      email: {
+        type: String,
+        required: true,
+      },
+      message: {
+        type: String,
+      },
+    },
+  ],
+});
+
+module.exports = Profile = mongoose.model('profile', profileSchema);
---
+++ models/User.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  userName: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = User = mongoose.model('user', userSchema);
